76015a217772654780915956e5d178bd5edcd977
Mohd Ryan Obillah <ryanobillah@gmail.com>
2022-06-22 11:46:27 +0700

    add Course

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,19 +1,21 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Discussion from './pages/Discussion'
-import Home from './pages/Home'
-import Log from './pages/Log'
-import Login from './pages/Login'
-import Profile from './pages/Profile'
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Course from './pages/Course';
+import Discussion from './pages/Discussion';
+import Home from './pages/Home';
+import Log from './pages/Log';
+import Login from './pages/Login';
+import Profile from './pages/Profile';
 
 export default function Main() {
-    return (
-        <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/" element={<Home />} />
-            <Route path='/test' element={<Log/>} />
-            <Route path="/profile" element={<Profile/>} />
-            <Route path="/discussion" element={<Discussion/>} />
-        </Routes>
-    )
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/test" element={<Log />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/discussion" element={<Discussion />} />
+      <Route pat="/course" element={<Course />} />
+    </Routes>
+  );
 }
