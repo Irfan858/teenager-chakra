32d86fe7a44ffbc13f82faefe51d2f6a95d3f1d4
Mohd Ryan Obillah <ryanobillah@gmail.com>
2022-06-23 18:47:10 +0700

    fix Discussion page and DiscussionCard

--- src/components/DiscussionCard.jsx
+++ src/components/DiscussionCard.jsx
@@ -1,18 +1,93 @@
-import React from 'react'
-import { Flex, Stack, Text, Spacer, Button } from '@chakra-ui/react'
+import React from 'react';
+import {
+  Box,
+  Flex,
+  Stack,
+  Text,
+  Spacer,
+  Button,
+  useDisclosure,
+  FormControl,
+  FormLabel,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Textarea,
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+  Input,
+} from '@chakra-ui/react';
 
-export default function DiscussionCard({title, module, moduleClass}) {
-    return (
-        <Flex bgColor="blue.300" p={4} width="full" height={24} borderRadius="10">
-            <Flex direction="column" alignContent="center">
-                <Text as="h1" fontsize="xl" fontWeight="semibold" color="white">{title}</Text>
-                <Stack direction="row">
-                    <Text as="span" fontsize="md" fontWeight="semibold" color="black">{module}</Text>
-                    <Text as="span" fontsize="md" fontWeight="semibold" color="black">{moduleClass}</Text>
-                </Stack>
-            </Flex>
-            <Spacer />
-            <Button mt={3} colorScheme="gray" size="sm" variant="solid">Lihat Semua</Button>
+export default function DiscussionCard({ question, module, moduleClass, answerNum, answer }) {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const initialRef = React.useRef(null);
+  return (
+    <Box width="full">
+      <Flex bgColor="blue.300" p={4} width="full" height={24} borderRadius="10">
+        <Flex direction="column" alignContent="center">
+          <Text as="h1" fontsize="4xl" fontWeight="semibold" color="white">
+            {question}
+          </Text>
+          <Stack direction="row">
+            <Text as="span" fontsize="md" fontWeight="semibold" color="black">
+              {module}
+            </Text>
+            <Text as="span" fontsize="md" fontWeight="semibold" color="black">
+              {moduleClass}
+            </Text>
+          </Stack>
         </Flex>
-    )
+        <Spacer />
+        <Button onClick={onOpen} mt={3} colorScheme="gray" size="sm" variant="solid">
+          Answer
+        </Button>
+        <Modal initialFocusRef={initialRef} isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Answer</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody pb={6}>
+              <FormControl mt={4}>
+                <FormLabel>Answer Number</FormLabel>
+                <Input placeholder="" />
+              </FormControl>
+
+              <FormControl>
+                <FormLabel>Answer</FormLabel>
+                <Textarea ref={initialRef} placeholder="Jawaban" />
+              </FormControl>
+            </ModalBody>
+            <ModalFooter>
+              <Button colorScheme="blue" mr={3}>
+                Tambah
+              </Button>
+              <Button onClick={onClose}>Batal</Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+      </Flex>
+      <Box>
+        <Accordion allowToggle>
+          <AccordionItem>
+            <h2>
+              <AccordionButton>
+                <Box flex="1" textAlign="left">
+                  {answerNum}
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+            </h2>
+            <AccordionPanel pb={4}>{answer}</AccordionPanel>
+          </AccordionItem>
+        </Accordion>
+      </Box>
+    </Box>
+  );
 }
--- src/pages/Discussion.jsx
+++ src/pages/Discussion.jsx
@@ -1,67 +1,107 @@
-import React from 'react'
-import { Box, Flex, Stack, VStack, Text, Spacer, Button } from '@chakra-ui/react'
-import Navbar from '../components/Navbar'
-import Sidebar from '../components/Sidebar'
+import React from 'react';
+import { Box, Flex, Stack, VStack, Spacer, FormControl, FormLabel, Input, Button, useDisclosure, Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, Textarea } from '@chakra-ui/react';
+import Navbar from '../components/Navbar';
+import Sidebar from '../components/Sidebar';
 // import CourseCard from '../components/CourseCard'
 // import SubmissionCard from '../components/SubmissionCard'
-import DiscussionCard from '../components/DiscussionCard'
+import DiscussionCard from '../components/DiscussionCard';
 
 let discussionList = [
-    {
-        id: 1,
-        title: "Apa Saja Struktur Bumi",
-        module: "Geografi",
-        class: "X IPS"
-    },
-    {
-        id: 2,
-        title: "Menentukan Asam Basa",
-        module: "Kimia",
-        class: "X IPA"
-    }
-]
+  {
+    id: 1,
+    question: 'Apa Saja Struktur Lapisan Bumi',
+    module: 'Geografi',
+    class: 'X IPS',
+    num: 'Jawaban 1',
+    answer: 'Crust (Kerak Bumi), Mantle (Mantel Bumi), Outer Core (Inti Luar), Inner Core / Inti Dalam',
+  },
+  {
+    id: 2,
+    question: 'Menentukan Asam Basa',
+    module: 'Kimia',
+    class: 'X IPA',
+  },
+  {
+    id: 3,
+    question: 'Menentukan Asam Basa',
+    module: 'Kimia',
+    class: 'X IPA',
+  },
+];
 
 export default function Discussion() {
-    return (
-        <>
-            <Navbar />
-            <Flex direction="row" justifyContent="flex-start" alignItems="flex-start" top="30">
-                {/* Sidebar */}
-                <Flex width="20%" minHeight="100vh" bgColor="grey.100" boxShadow='md' position="fixed" left="0" top="20" overflowY="auto">
-                    <Sidebar />
-                </Flex>
-                {/* End Sidebar */}
-                {/* Main */}
-                <Flex direction="column" width="80%" minHeight="90vh" bg="white" position="sticky" left="80" marginTop={20}>
-                    <Box m={5}>
-                        <Stack spacing={6}>
-                            {/* Header */}
-                            <Box>
-                                <Box as="h1" fontSize="2xl" fontWeight="semibold">
-                                    Diskusi
-                                </Box>
-                                <Box as="span" fontSize="l" fontWeight="semibold" color="grey">
-                                    Tanyakan Dan Temukan Jawaban pertanyaan Anda Bersama Teman yang Lain
-                                </Box>
-                            </Box>
-                            {/* End Header */}
-                            {/* Content */}
-                            <Button colorScheme="blue" variant="solid" width="full">Tambah Diskusi</Button>
-                            <Box alignContent="flex-start" >
-                                <VStack spacing={8}>
-                                    {
-                                        discussionList.map((discussion, index) => {
-                                            return <DiscussionCard key={index} title={discussion.title} module={discussion.module} moduleClass={discussion.class} />
-                                        })
-                                    }
-                                </VStack>
-                            </Box>
-                            {/* End Content */}
-                        </Stack>
-                    </Box>
-                </Flex>
-                {/* End main */}
-            </Flex>
-        </>
-    )
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const initialRef = React.useRef(null);
+  return (
+    <>
+      <Navbar />
+      <Flex direction="row" justifyContent="flex-start" alignItems="flex-start" top="30">
+        {/* Sidebar */}
+        <Flex width="20%" minHeight="100vh" bgColor="grey.100" boxShadow="md" position="fixed" left="0" top="20" overflowY="auto">
+          <Sidebar />
+        </Flex>
+        {/* End Sidebar */}
+        {/* Main */}
+        <Flex direction="column" width="80%" minHeight="90vh" bg="white" position="sticky" left="80" marginTop={20}>
+          <Box m={5}>
+            <Stack spacing={6}>
+              {/* Header */}
+              <Box>
+                <Box as="h1" fontSize="2xl" fontWeight="semibold">
+                  Diskusi
+                </Box>
+                <Box as="span" fontSize="l" fontWeight="semibold" color="grey">
+                  Tanyakan Dan Temukan Jawaban pertanyaan Anda Bersama Teman yang Lain
+                </Box>
+              </Box>
+              {/* End Header */}
+              {/* Content */}
+              <Button onClick={onOpen} colorScheme="blue" variant="solid" width="full">
+                Tambah Diskusi
+              </Button>
+              <Modal initialFocusRef={initialRef} isOpen={isOpen} onClose={onClose}>
+                <ModalOverlay />
+                <ModalContent>
+                  <ModalHeader>Buat Pertanyaan</ModalHeader>
+                  <ModalCloseButton />
+                  <ModalBody pb={6}>
+                    <FormControl>
+                      <FormLabel>Question</FormLabel>
+                      <Textarea ref={initialRef} placeholder="Pertanyaan" />
+                    </FormControl>
+
+                    <FormControl mt={4}>
+                      <FormLabel>Module</FormLabel>
+                      <Input placeholder="Module" />
+                    </FormControl>
+
+                    <FormControl mt={4}>
+                      <FormLabel>Kelas</FormLabel>
+                      <Input placeholder="Kelas" />
+                    </FormControl>
+                  </ModalBody>
+
+                  <ModalFooter>
+                    <Button colorScheme="blue" mr={3}>
+                      Tambah
+                    </Button>
+                    <Button onClick={onClose}>Batal</Button>
+                  </ModalFooter>
+                </ModalContent>
+              </Modal>
+              <Box alignContent="flex-start">
+                <VStack spacing={8}>
+                  {discussionList.map((discussion, index) => {
+                    return <DiscussionCard key={index} question={discussion.question} module={discussion.module} moduleClass={discussion.class} answerNum={discussion.num} answer={discussion.answer} />;
+                  })}
+                </VStack>
+              </Box>
+              {/* End Content */}
+            </Stack>
+          </Box>
+        </Flex>
+        {/* End main */}
+      </Flex>
+    </>
+  );
 }
