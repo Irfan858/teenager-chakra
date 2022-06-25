49c94213f8c8371d494d245c3984b9a1871cad79
Mohd Ryan Obillah <ryanobillah@gmail.com>
2022-06-25 16:53:10 +0700

    Fix Discussion Page and add Anwer Page

--- src/Main.jsx
+++ src/Main.jsx
@@ -7,6 +7,7 @@ import Home from './pages/Home';
 import Log from './pages/Log';
 import Login from './pages/Login';
 import Profile from './pages/Profile';
+import Answer from './pages/Answer';
 import Submission from './pages/Submission';
 
 export default function Main() {
@@ -19,6 +20,7 @@ export default function Main() {
       <Route path="/discussion" element={<Discussion />} />
       <Route path="/course" element={<Course />} />
       <Route path="/coursedetail" element={<CourseDetail />} />
+      <Route path="/answer" element={<Answer />} />
     </Routes>
   );
 }
